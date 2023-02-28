1b80da7e16ef143bb3b333a6d38ce7f36d3fe1bf
mukesh bagaria <mukeshbagaria@mukeshs-MacBook-Air.local>
2023-02-28 22:53:19 +0530

    Setup Basic Project with express server

---
+++ src/index.js
@@ -0,0 +1,17 @@
+const express=require('express');
+const bodyParser=require('body-parser');
+
+const setupAndStartServer= async () => {
+    const app=express();
+    
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({extended:true}));
+
+    app.listen(3000 ,()=>{
+        console.log(`server started on port 3000`);
+
+    })  
+}   
+
+
+setupAndStartServer();
